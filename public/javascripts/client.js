9dbfb04d32b9d09550a44017f5cbbb4d77faa064
eradatiano <eradatiano@gmail.com>
2024-10-14 00:59:26 +0330

    fix another route error

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -8,7 +8,7 @@ window.addEventListener("DOMContentLoaded", function () {
   const loginBtn = document.querySelector(".login-btn");
   const usernameInput = document.querySelector(".username-input");
   const passwordInput = document.querySelector(".pass-input");
-  const baseUrl = "https://chatmir-v001.vercel.app";
+  const baseUrl = "https://chatmir-v001.vercel.app/";
 
   // display chat-box
   const displayChat = () => {
